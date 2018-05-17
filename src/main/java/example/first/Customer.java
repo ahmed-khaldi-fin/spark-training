dd0c7ce743819a29252650f8f47666ef0b881c7d
ahmed-khaldi-fin <akhaldi@finaxys.com>
2018-05-17 13:45:30 +0200

    - spark context

--- src/main/java/example/first/Customer.java
+++ src/main/java/example/first/Customer.java
@@ -1,4 +1,7 @@
 package example.first;
 
+import org.apache.spark.sql.SparkSession;
+
 public class Customer {
+    SparkSession session = SparkSession.builder().master("local[*]").appName("mohamed-example").getOrCreate();
 }
