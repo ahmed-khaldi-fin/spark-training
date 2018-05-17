cc0587007a4c47dcc4d511ecb21dc6af8a69e764
ahmed-khaldi-fin <akhaldi@finaxys.com>
2018-05-17 14:24:11 +0200

    - read from file comptes

--- src/main/java/first/example/SparkExample.java
+++ src/main/java/first/example/SparkExample.java
@@ -1,19 +1,20 @@
 package first.example;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 public class SparkExample {
 
-	public static void main(String[] args) {
-		
-		SparkSession spark = SparkSession.builder().master("local[*]").appName("spark-job").getOrCreate();
-		spark.sql("set spark.sql.orc.impl=native");
-		
-		Dataset<Row> comptes = spark.read().option("header", "false").csv("mohamed-example/compte.csv");
+    public static void main(String[] args) {
+
+        SparkSession spark = SparkSession.builder().master("local[*]").appName("spark-job").getOrCreate();
+        spark.sql("set spark.sql.orc.impl=native");
+
+        Dataset<Row> comptes = spark.read().option("header", "true").option("sep",";").csv("mohamed-example/compte.csv");
+        Encoder<Compte> compteEncoder = Encoders.bean(Compte.class);
+        Dataset<Compte> compteDF = comptes.as(compteEncoder);
+        compteDF.show();
 
 
-	}
+    }
 
 }
