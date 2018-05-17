f44f476434a15c35458ee91a88f8b77182465ad0
finaxys <cblampain@finaxys.com>
2018-05-17 13:50:16 +0200

    Started example in eclipse

---
+++ src/main/java/SparkExample.java
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkExample {
+
+	public static void main(String[] args) {
+		
+		SparkSession spark = SparkSession.builder().master("local[*]").appName("spark-job").getOrCreate();
+		spark.sql("set spark.sql.orc.impl=native");
+		
+		Dataset<Row> df = spark.read().option("header", "false").csv("mohamed-example/compte.csv");
+
+	}
+
+}
--- src/main/java/example/first/Customer.java
+++
@@ -1,4 +0,0 @@
-package example.first;
-
-public class Customer {
-}
