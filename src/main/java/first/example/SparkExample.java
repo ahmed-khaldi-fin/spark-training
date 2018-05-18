ad0ae133d875902b385104910985e468379d925e
ahmed-khaldi-fin <akhaldi@finaxys.com>
2018-05-18 14:05:46 +0200

    - join

--- src/main/java/first/example/SparkExample.java
+++ src/main/java/first/example/SparkExample.java
@@ -11,19 +11,24 @@ public class SparkExample {
 
     public static void main(String[] args) {
 
+        String idCostumer = "id_costumer";
+        String montantTransaction = "montant_transaction";
         SparkSession spark = SparkSession.builder().master("local[*]").appName("spark-job").getOrCreate();
         spark.sql("set spark.sql.orc.impl=native");
 
         Dataset<Compte> comptes = spark.read().option("header", "true").option("sep", ";")
                                        .csv("mohamed-example/compte.csv").as(Encoders.bean(Compte.class));
 
-        String idCostumer = "id_costumer";
-        String montantTransaction = "montant_transaction";
-        Dataset<Row> res1 = comptes.select(idCostumer, "type_compte", montantTransaction)
-                                   .groupBy(idCostumer, "type_compte")
+        Dataset<Customer> customers = spark.read().option("header", "true").option("sep", ";")
+                                       .csv("mohamed-example/customer.csv").as(Encoders.bean(Customer.class));
+        Dataset<Row> res1 = customers.join(comptes, idCostumer);
+
+
+        Dataset<Row> res2 = res1.select(idCostumer, "type_compte", montantTransaction, "first_name")
+                                   .groupBy(idCostumer, "type_compte","first_name")
                                    .agg(sum(montantTransaction).alias("total_transaction"));
 
-        Dataset<Row> res2 = comptes
+        Dataset<Row> res3 = comptes
                 .select(col(idCostumer),
                         year(col("date_transaction")),
                         col(montantTransaction)).groupBy(idCostumer, "year(date_transaction)")
@@ -31,6 +36,7 @@ public class SparkExample {
 
         res1.show();
         res2.show();
+        res3.show();
     }
 
 }
